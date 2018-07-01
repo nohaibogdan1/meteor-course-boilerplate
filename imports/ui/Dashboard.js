317f0499d8bb738aa0952a8d0ed6fc4f02d64d91
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-07-01 22:20:37 +0300

    Init commit

---
+++ imports/ui/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import PrivateHeader from './PrivateHeader';
+
+
+const Dashboard = () =>  {
+	return (
+		<div>
+			<PrivateHeader title="Dashboard"/>
+			<div className="page-content">
+				Dashboard page content
+			</div>
+		</div>
+		
+	);
+}
+
+export default Dashboard;
